c2dbbb9f0cc2d71c3b245cb9f504bbb3f92326c6
Gayane Nerkararyan <imagin1@yandex.ru>
2024-06-25 16:48:10 +0400

    - Fixed app build issues.

--- DemoPublishLibrary/settings.gradle.kts
+++ DemoPublishLibrary/settings.gradle.kts
@@ -10,6 +10,14 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            name = "GithubPackages"
+            url = uri("https://maven.pkg.github.com/Galateyta/PublishLibrary")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
 }
 
